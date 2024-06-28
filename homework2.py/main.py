1f3f29aa0e9027e352174dba3f036a5ea8fa07b9
Proscka <proskinaviktoria861@gmail.com>
2024-06-28 15:47:46 +0300

    Add files via upload

---
+++ homework2.py/main.py
@@ -0,0 +1,14 @@
+a={"Количество выполненных заданий":12}
+print(a)
+age=12
+print(age)
+b={"Количество затраченных часов":1.5}
+print(b)
+age=1.5
+print(age)
+name="Python"
+print(name)
+print(sum([1.5/12]))
+name="Курс_Python,problem,всего:12,hour,затраченно,часов:1.5,float,среднее время,выполнения:0.125 часа"
+print(name)
+
